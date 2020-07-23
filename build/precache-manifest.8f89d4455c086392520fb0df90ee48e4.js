f09ab8da9f0ea2ececd55c268bc4e646dfcf9a0e
Rohan Mandal <65089293+rohanmandal99@users.noreply.github.com>
2020-07-23 20:36:56 +0530

    Add files via upload

---
+++ build/precache-manifest.8f89d4455c086392520fb0df90ee48e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a2f5119fdcb249a439c0dbd6f8cb3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f1c3bc54aedfed8059",
+    "url": "/static/css/2.b756e824.chunk.css"
+  },
+  {
+    "revision": "b4a058978786ed49abc9",
+    "url": "/static/css/main.bde5a17c.chunk.css"
+  },
+  {
+    "revision": "a1f1c3bc54aedfed8059",
+    "url": "/static/js/2.6437bf2f.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.6437bf2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a058978786ed49abc9",
+    "url": "/static/js/main.65e388f1.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
